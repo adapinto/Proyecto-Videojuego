d71b0c378d7897432087b50ceeb301d082807b62
adapinto <60575727+adapinto@users.noreply.github.com>
2020-03-25 18:09:57 -0600

    Cuarto commit

--- src/Implementacion/Juego.java
+++ src/Implementacion/Juego.java
@@ -1,5 +1,8 @@
 package Implementacion;
 
+import java.util.HashMap;
+
+import Clases.Jugador;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
@@ -11,78 +14,120 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
-public class Juego extends Application {
-		private Group root;
-		private Scene escena;
-		private Canvas lienzo;
-		private GraphicsContext graficos;
-		private int x=0;
-	
-	public static void main (String [] args) {
-	launch (args);	
+public class Juego extends Application{
+	private GraphicsContext graficos;
+	private Group root;
+	private Scene escena;
+	private Canvas lienzo;
+	private Jugador jugador;
+	public static boolean arriba;
+	public static boolean abajo;
+	public static boolean izquierda;
+	public static boolean derecha;
+	public static HashMap<String, Image> imagenes;
+
+	public static void main(String[] args) {
+		launch(args);
 	}
-	
 
 	@Override
 	public void start(Stage ventana) throws Exception {
 		inicializarComponentes();
 		gestionEventos();
 		ventana.setScene(escena);
-		ventana.setTitle("Oppositi Complementari");
+		ventana.setTitle("Oppsiti Complementari");
 		ventana.show();
 		cicloJuego();
-		
 	}
 	
 	public void cicloJuego() {
-		long tiempoInicial=System.nanoTime();
-		AnimationTimer animationTimer= new AnimationTimer() {
+		long tiempoInicial = System.nanoTime();
+		AnimationTimer animationTimer = new AnimationTimer() {
 
 			@Override
 			public void handle(long tiempoActual) {
-				double t=(tiempoActual-tiempoInicial)/1000000000.0;
+				double t = (tiempoActual - tiempoInicial) / 1000000000.0;
+				System.out.println(t);
 				actualizarEstado();
 				pintar();
 			}
 			
 		};
+		
 		animationTimer.start();
-		}
+	}
 	
 	public void actualizarEstado() {
-		
+		jugador.mover();
 	}
 	
 	public void inicializarComponentes() {
-		root= new Group();
-		escena=new Scene(root, 700,500);
-		lienzo=new Canvas(700,500);
+		imagenes = new HashMap<String,Image>();
+		cargarImagenes();
+		jugador = new Jugador(20, 325, 3, "personaje");
+		root = new Group();
+		escena = new Scene(root, 700, 500);
+		lienzo = new Canvas(700, 500);
 		root.getChildren().add(lienzo);
-		graficos=lienzo.getGraphicsContext2D();
+		graficos = lienzo.getGraphicsContext2D();
 	}
 	
+	public void cargarImagenes() {
+		imagenes.put("personaje", new Image("personaje.png"));
+	}
 	
 	public void pintar() {
-		graficos.fillRect(0, 0,700, 500);
-		graficos.drawImage(new Image("personaje.png"),x,0);
+		graficos.drawImage(new Image("fondo.jpg"), 0, 0);
+		jugador.pintar(graficos);
 	}
-	
-	
 	
 	public void gestionEventos() {
 		escena.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
 			@Override
 			public void handle(KeyEvent evento) {
-				switch (evento.getCode().toString()) {
-				case "RIGHT":
-					x+=10;
-					break;
+				switch(evento.getCode().toString()) {
+					case "RIGHT":
+						derecha = true;
+						break;
+					case "LEFT":
+						izquierda = true;
+						break;
+					case "UP":
+						arriba = true;
+						break;
+					case "DOWN":
+						abajo = true;
+					case "SPACE":
+						jugador.setVelocidad(15);
+						break;
 				}
-			}	
+			}
 		});
+		
+		escena.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+				public void handle(KeyEvent evento) {
+					switch(evento.getCode().toString()) {
+					case "RIGHT":
+						derecha = false;
+						break;
+					case "LEFT":
+						izquierda = false;
+						break;
+					case "UP":
+						arriba = false;
+						break;
+					case "DOWN":
+						abajo = false;
+					case "SPACE":
+						jugador.setVelocidad(5);
+				}
+			}
 			
-		
-		
+		});
 	}
 	
+	
+
 }
